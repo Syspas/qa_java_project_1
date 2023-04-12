b6a0aab353e17ba3635bc465a8bb637485d812ed
TestMasterBest <cyberportal.info@gmail.com>
2023-04-12 21:31:22 +0300

    Добавляем //Загружаем веб драйвер из GIT WebDriverManager.firefoxdriver().setup(); //Для быстрого ввода текста в поля Configuration.fastSetValue = true; //Запускать скрытый браузер Configuration.headless=true;

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,5 +1,8 @@
 import com.codeborne.selenide.Configuration;
 import static com.codeborne.selenide.Browsers.FIREFOX;
+
+import com.codeborne.selenide.WebDriverRunner;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Before;
 
 
@@ -7,9 +10,18 @@ public class BaseTest {
     @Before
     public void setBrowser()
     {
+        //Загружаем веб драйвер из GIT
+        WebDriverManager.firefoxdriver().setup();
+
+
         //Запускаем тест в браузере FIREFOX
-       Configuration.browser = FIREFOX ;
-        Configuration.browserSize = "1920x1080";
+       Configuration.browser ="firefox" ;
+       Configuration.browserSize = "1920x1080";
+        //Для быстрого ввода текста в поля
+        Configuration.fastSetValue = true;
+        //Запускать скрытый браузер
+        Configuration.headless=true;
+
 
     }
 
